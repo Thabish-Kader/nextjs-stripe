080fcec29ce3ddbaa07bee05ca7d54e00a0c7343
Thabish <kadertabish2@gmail.com>
2023-05-21 10:39:48 +0400

    stripe
    Signed-off-by: Thabish <kadertabish2@gmail.com>

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import { FormEvent } from "react";
 
 export default function Home() {
+	const handleSubmit = (event: FormEvent<HTMLFormElement>) => {};
+
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+		<main className="flex min-h-screen flex-col items-center justify-between p-24">
+			<form onSubmit={handleSubmit} method="POST">
+				<button type="submit">Checkout</button>
+			</form>
+		</main>
 	);
 }
